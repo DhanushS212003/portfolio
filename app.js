d7f97cecb865ee7ce4c7aa9b2ede8c222d99839c
DhanushS212003 <dhanush.selvam@fwsa.freshworks.com>
2023-09-12 15:52:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+function validateForm() {
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let subject = document.getElementById("subject").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  console.log(name, email, subject, message);
+      
+}
